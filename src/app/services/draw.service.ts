55a66991b6785111cdcf15b12e752cd184cd6e41
chinaglia-rafa <chinaglia.rafa@gmail.com>
2022-11-28 19:09:16 -0300

    WIP: projeções

--- src/app/services/draw.service.ts
+++ src/app/services/draw.service.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@angular/core';
 import { ImageService } from './image.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DrawService {
-
-  constructor(public imageService: ImageService) { }
+  constructor(public imageService: ImageService) {}
 
   public currentTool = 0;
-  
-  draw(arrX = [], arrY = []){
-    if(this.currentTool == 0){
-      this.imageService.colorPoint(arrX[1],arrY[1]);
-    }else if(this.currentTool == 1){
+
+  draw(arrX = [], arrY = []) {
+    if (this.currentTool == 0) {
+      this.imageService.colorPoint(arrX[1], arrY[1]);
+    } else if (this.currentTool == 1) {
       this.imageService.drawLinePar(arrX, arrY);
-    }else if(this.currentTool == 2){
-      // this.imageService.drawCircleEq(arrX, arrY);
-      this.imageService.drawCirclePar(arrX, arrY);
+    } else if (this.currentTool == 2) {
+      this.imageService.drawCircleEq(arrX, arrY);
+      //this.imageService.drawCirclePar(arrX, arrY);
     }
   }
-
 }
